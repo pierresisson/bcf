e43796ee8c3735c01395992d949d9c2d8aff6aa0
pierresisson <79455860+pierresisson@users.noreply.github.com>
2024-08-11 18:13:26 +0200

    improve profile and hp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ import { Button } from '@/components/ui/button';
 export default function HomePage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold mb-8">Bienvenue sur HarmonIQ</h1>
+      <h1 className="text-4xl font-bold mb-8">Welcome on harmonIQ!</h1>
       <Link href="/profile" passHref>
-        <Button size="lg">Accéder au Dashboard</Button>
+        <Button size="lg">Go to Profile page</Button>
       </Link>
     </div>
   );
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ProfileForm from '../../components/ProfileForm';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import { ChevronLeft, ChevronRight } from 'lucide-react';
 
 export default function Profile() {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
+      <Link href="/" passHref>
+        <Button variant="link">
+          <ChevronLeft className="h-4 w-4" />
+          Go to dashboard
+        </Button>
+      </Link>
       <ProfileForm />
     </div>
   );
