8ce4ba488d221cdc2e8058a824b5e255430678c9
pierresisson <79455860+pierresisson@users.noreply.github.com>
2024-08-11 19:09:51 +0200

    clean

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -8,8 +8,6 @@ import { createClient } from '@/utils/supabase/server';
 export async function login(formData: FormData) {
   const supabase = createClient();
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
   const data = {
     email: formData.get('email') as string,
     password: formData.get('password') as string,
@@ -18,7 +16,6 @@ export async function login(formData: FormData) {
   const { error } = await supabase.auth.signInWithPassword(data);
 
   if (error) {
-    console.log('1', error);
     redirect('/error');
   }
 
@@ -39,7 +36,6 @@ export async function signup(formData: FormData) {
   const { error } = await supabase.auth.signUp(data);
 
   if (error) {
-    console.log('2', error);
     redirect('/error');
   }
 
